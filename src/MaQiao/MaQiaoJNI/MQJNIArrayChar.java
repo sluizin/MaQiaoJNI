b23f0e734a64134c092e4e07adfaacbf001925e8
sunjian <sluizin@sohu.com>
2015-07-27 18:24:42 +0800

    2015-7-27 熟悉c++程序运行 但没有办法输出到java上

--- src/MaQiao/MaQiaoJNI/MQJNIArrayChar.java
+++ src/MaQiao/MaQiaoJNI/MQJNIArrayChar.java
@@ -1,7 +1,7 @@
 package MaQiao.MaQiaoJNI;
 public final class MQJNIArrayChar {
 	static {
-		System.loadLibrary("MQJNIArrayChar");// 装入动态链接库，"HelloWorldImpl"是要装入的动态链接库名称。
+		System.loadLibrary("MQJNIArrayChar");// 装入动态链接库，"MQJNIArrayChar"是要装入的动态链接库名称。
 	}
 	public static final native char[] coupling(final char... array);
 	public static final native char[] coupling(final String str);
